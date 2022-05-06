8f9efc2af3a0ce82e59897d9119a6ae5e1076679
anzar <anzabbas@icloud.com>
2022-05-06 16:13:42 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+# Setting up
+setup(
+  name="openwillis",
+  version="0.1",
+  author="bklynhlth",
+  author_email="<admin@bklynhlth.com>",
+  description="openwillis",
+  long_description="open source python package for digital measurement of health from behavioral data",
+  packages=find_packages(),
+  install_requires=[],
+  keywords=['digital biomarkers', 'digital phenotyping'],
+  classifiers=[]
+)
