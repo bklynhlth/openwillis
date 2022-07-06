f0d169a5d136d2759f2b4d57171e6135d6e945e2
vjbytes102 <vy386@nyu.edu>
2022-07-06 08:58:05 -0400

    version update

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='openwillis',
-                 version='0.2',
+                 version='0.0.1',
                  description='digital health measurement',
                  long_description=long_description,
                  long_description_content_type="text/markdown",
