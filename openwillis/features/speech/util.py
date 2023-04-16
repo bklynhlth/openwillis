03b716bd3ebd773f44cc9592029420b7f433a903
kunal kumar kushwaha <kunalkumar.kus@gmail.com>
2023-04-16 17:07:37 +0530

    add ssl configuration for package download

--- openwillis/features/speech/util.py
+++ openwillis/features/speech/util.py
@@ -13,6 +13,7 @@ from pydub import AudioSegment
 import logging
 
 import nltk
+import ssl
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from lexicalrichness import LexicalRichness
 
@@ -22,6 +23,13 @@ from openwillis.features.speech import speech_transcribe as stranscribe
 
 logging.basicConfig(level=logging.INFO)
 logger=logging.getLogger()
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
 
 #NLTK Tag list
 tag_dict = {'PRP': 'Pronoun', 'PRP$': 'Pronoun', 'VB': 'Verb', 'VBD': 'Verb', 'VBG': 'Verb' , 'VBN': 'Verb',
