df07f5022293015f7f2d4ee3493f1c4582a5f631
Vijay Yadav <vijayyadav@vijays-mbp.myfiosgateway.com>
2023-10-13 13:32:38 -0400

    speech characteristic fix

--- openwillis/measures/text/util/characteristics_util.py
+++ openwillis/measures/text/util/characteristics_util.py
@@ -472,18 +472,21 @@ def filter_phrases(item_data, speaker_label, measures):
 
     phrases_idxs, phrases = [], []
     for item in item_data:
+        try:
 
-        start_idx = item["words"][0][measures["old_index"]]
-        end_idx = item["words"][-1][measures["old_index"]]
+            start_idx = item["words"][0][measures["old_index"]]
+            end_idx = item["words"][-1][measures["old_index"]]
 
-        if speaker_label is not None:
-            if item["speaker"] == speaker_label:
+            if speaker_label is not None:
+                if item["speaker"] == speaker_label:
+                    phrases.append(item["text"])
+                    phrases_idxs.append((start_idx, end_idx))
+            else:
                 phrases.append(item["text"])
                 phrases_idxs.append((start_idx, end_idx))
-        else:
-            phrases.append(item["text"])
-            phrases_idxs.append((start_idx, end_idx))
 
+        except Exception as e:
+            logger.error(f"Failed to filter phrases: {e}")
     return phrases_idxs, phrases
 
 
