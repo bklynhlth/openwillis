b29b294505456a8d294d0899f27e80334f4d23b8
vjbytes102 <vy386@nyu.edu>
2023-10-24 00:41:37 -0400

    Update separation_util.py

--- openwillis/measures/audio/util/separation_util.py
+++ openwillis/measures/audio/util/separation_util.py
@@ -455,7 +455,7 @@ def transcribe_response_to_dataframe(response):
 
                 df = df[df["confidence"] > 0].reset_index(drop=True)
                 df = df[["start_time", "end_time", "confidence", "speaker_label", "content"]]
-                df = df.dropna()
+                df = df.dropna().reset_index(drop=true)
                 
     return df, speakers
 
