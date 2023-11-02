8078d7bc8b8227d9855d346af2691892eb4fea6e
vjbytes102 <vy386@nyu.edu>
2023-11-01 23:37:14 -0400

    Update characteristics_util
    Co-authored-by: GeorgiosEfstathiadis <54844705+GeorgeEfstathiadis@users.noreply.github.com>

--- openwillis/measures/text/util/characteristics_util.py
+++ openwillis/measures/text/util/characteristics_util.py
@@ -642,7 +642,10 @@ def update_summ_df(df_diff, summ_df, full_text, time_index, word_df, turn_df, me
 
     ------------------------------------------------------------------------------------------------------
     """
-    speech_minutes = (float(df_diff.iloc[-1][time_index[1]]) - float(df_diff.iloc[0][time_index[0]])) / 60
+    if len(turn_df) > 0:
+        speech_minutes = turn_df[measures["turn_minutes"]].sum()
+    else:
+        speech_minutes = (float(df_diff.iloc[-1][time_index[1]]) - float(df_diff.iloc[0][time_index[0]])) / 60
     summ_df[measures["speech_minutes"]] = [speech_minutes]
     
     summ_df[measures["speech_words"]] = len(df_diff)
