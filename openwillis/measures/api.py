53cce2fb1487a363f02da1c89b387f24547277b1
GeorgiosEfstathiadis <gioefst2000@gmail.com>
2024-05-09 09:52:06 -0400

    import diarization correction

--- openwillis/measures/api.py
+++ openwillis/measures/api.py
@@ -16,7 +16,8 @@ from openwillis.measures.audio import (
     speech_transcription_vosk
 )
 from openwillis.measures.text import (
-    speech_characteristics
+    speech_characteristics,
+    diarization_correction
 )
 from openwillis.measures.gps import (
     gps_analysis
--- openwillis/measures/text/__init__.py
+++ openwillis/measures/text/__init__.py
@@ -2,4 +2,8 @@ from openwillis.measures.text.speech_attribute import (
     speech_characteristics,
 )
 
-__all__ = ["speech_characteristics"]
+from openwillis.measures.text.llm_diarization import (
+    diarization_correction,
+)
+
+__all__ = ["speech_characteristics", "diarization_correction"]
