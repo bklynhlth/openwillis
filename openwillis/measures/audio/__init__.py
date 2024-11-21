4d79db2eb9dc45c8a08ee0b5782620634a78edc1
GeorgiosEfstathiadis <gioefst2000@gmail.com>
2024-11-21 08:54:10 -0500

    add phonation_acoustics to init

--- openwillis/measures/audio/__init__.py
+++ openwillis/measures/audio/__init__.py
@@ -26,4 +26,8 @@ from openwillis.measures.audio.speech_preprocess import (
     audio_preprocess,
 )
 
-__all__ = ["vocal_acoustics", "speech_transcription_whisper", "speaker_separation", "speaker_separation_cloud", "speech_transcription_aws", "speech_transcription_vosk", "audio_preprocess"]
+from openwillis.measures.audio.speech_phonation import (
+    phonation_acoustics,
+)
+
+__all__ = ["vocal_acoustics", "speech_transcription_whisper", "speaker_separation", "speaker_separation_cloud", "speech_transcription_aws", "speech_transcription_vosk", "audio_preprocess", "phonation_acoustics"]
