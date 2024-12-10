7a676b34063cfc7b2375981a6413640187170fe7
GeorgiosEfstathiadis <gioefst2000@gmail.com>
2024-12-10 15:12:33 -0500

    code for openwillis-speech

---
+++ openwillis-speech/src/openwillis/speech/__init__.py
@@ -0,0 +1,13 @@
+from openwillis.measures.text.speech_attribute import (
+    speech_characteristics,
+)
+
+from openwillis.measures.text.willisdiarize_aws import (
+    diarization_correction_aws,
+)
+
+from openwillis.measures.text.willisdiarize import (
+    diarization_correction
+)
+
+__all__ = ["speech_characteristics", "diarization_correction_aws", "diarization_correction"]
