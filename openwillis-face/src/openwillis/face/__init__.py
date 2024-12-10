968be22023119b4848e95898cfb6be84edb70b98
GeorgiosEfstathiadis <gioefst2000@gmail.com>
2024-12-10 14:49:57 -0500

    code for openwillis-face

---
+++ openwillis-face/src/openwillis/face/__init__.py
@@ -0,0 +1,20 @@
+from openwillis.measures.video.face_landmark import (
+    facial_expressivity,
+)
+from openwillis.measures.video.facial_emotion import (
+    emotional_expressivity,
+)
+
+from openwillis.measures.video.eye_blink import (
+    eye_blink_rate,
+)
+
+from openwillis.measures.video.preprocess_video import (
+    preprocess_face_video
+)
+
+from openwillis.measures.video.util import (
+    create_cropped_video
+)
+
+__all__ = ["facial_expressivity", "emotional_expressivity", "eye_blink_rate", "preprocess_face_video","create_cropped_video"]
