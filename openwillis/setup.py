b0983cfc02cf76954e9cff3555f0dbcb1cdfee6e
GeorgiosEfstathiadis <gioefst2000@gmail.com>
2024-12-12 16:19:51 -0500

    update version

--- openwillis/setup.py
+++ openwillis/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='openwillis',
-    version='3.0.0',
+    version='3.0.1',
     description='digital health measurement',
     long_description=long_description,
     long_description_content_type="text/markdown",
